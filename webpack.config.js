733ba51796d4c450ee9cac1d63509d2f19df6bb1
Travis Horn <travis@travishorn.com>
2019-02-26 13:26:51 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'session-timeout.js',
+    library: 'sessionTimeout',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      },
+    ],
+  },
+};
